cbe905ce9517bf31164a420c26cf0cb1130d88a5
YohanF1245 <yohan.ferdin@hotmail.fr>
2026-01-12 14:14:34 +0100

    feat(df1.py): Integrate D1Schema for data validation and update output to show data types

--- df1.py
+++ df1.py
@@ -1,5 +1,8 @@
 import pandas as pd 
-import pandera as pa 
+from df1_pandera_schema import D1Schema
+
 
 df = pd.read_csv('data/data_1.csv')
-print(df.head())
+print(df.dtypes)
+df_panderized = D1Schema.validate(df)
+print(df_panderized.dtypes)
