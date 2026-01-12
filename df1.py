05ba1f8616392a29dac818aa937f7e604eaf07b2
YohanF1245 <yohan.ferdin@hotmail.fr>
2026-01-12 14:46:36 +0100

    delete(df1.py): Remove data loading and validation script as it is no longer needed

--- df1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-from df1_pandera_schema import D1Schema
-
-
-df = pd.read_csv('data/data_1.csv')
-print(df.dtypes)
-df_panderized = D1Schema.validate(df)
-print(df_panderized.dtypes)
